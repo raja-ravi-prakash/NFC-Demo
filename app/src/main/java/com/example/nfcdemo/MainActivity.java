0ca5ef716536c4f14df242159b6a0ef119f4ad5d
raja-ravi-prakash <iamraviprakash1@gmail.com>
2019-09-27 00:14:24 +0530

    Added files

--- app/src/main/java/com/example/nfcdemo/MainActivity.java
+++ app/src/main/java/com/example/nfcdemo/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.nfcdemo;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.nfc.NdefMessage;
 import android.nfc.NdefRecord;
@@ -8,21 +9,28 @@ import android.nfc.NfcAdapter;
 import android.nfc.NfcEvent;
 import android.os.Bundle;
 import android.os.Parcelable;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.TextView;
 import android.widget.Toast;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import me.aflak.libraries.callback.FingerprintDialogCallback;
+import me.aflak.libraries.dialog.FingerprintDialog;
 
 import static android.nfc.NdefRecord.createMime;
 
-public class MainActivity extends Activity implements NfcAdapter.CreateNdefMessageCallback {
+public class MainActivity extends Activity implements NfcAdapter.CreateNdefMessageCallback , FingerprintDialogCallback {
     private NfcAdapter nfcAdapter;
     private EditText amountSend;
-    private Button send;
+    private Button send,add;
     private TextView walletValue;
+    private TextView info;
     private Integer amount=100;
     String text ;
+    boolean a=false;
+
+
 
 
     @Override
@@ -34,8 +42,41 @@ public class MainActivity extends Activity implements NfcAdapter.CreateNdefMessa
         amountSend=findViewById(R.id.editfield);
         send=findViewById(R.id.send);
         walletValue=findViewById(R.id.value);
+        add=findViewById(R.id.add);
+        info=findViewById(R.id.info);
+
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                closeKeyBoard();
+                amount+=Integer.parseInt(amountSend.getText().toString());
+                text=amount.toString();
+                walletValue.setText(text);
+            }
+        });
+
     }
     public void send(View v){
+        closeKeyBoard();
+        if(FingerprintDialog.isAvailable(this)) {
+            FingerprintDialog.initialize(this)
+                    .title("VERIFY")
+                    .message("Confirm Finger Print to Continue")
+                    .callback(this)
+                    .show();
+        }
+
+    }
+    @Override
+    public void onAuthenticationSucceeded() {
+        sendPrompt();
+    }
+
+    @Override
+    public void onAuthenticationCancel() {
+        info.setText("Authentication Failed");
+    }
+    public void sendPrompt(){
         text= amountSend.getText().toString();
         if(amount-Integer.parseInt(text)<0)
             Toast.makeText(this, "No Enough Amount", Toast.LENGTH_LONG).show();
@@ -89,4 +130,13 @@ public class MainActivity extends Activity implements NfcAdapter.CreateNdefMessa
        walletValue.setText(text);
 
     }
+    private void closeKeyBoard(){
+        View view=this.getCurrentFocus();
+
+        InputMethodManager im=(InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        im.hideSoftInputFromWindow(view.getWindowToken(),0);
+    }
+
+
+
 }
